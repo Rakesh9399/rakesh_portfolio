f3bc8b18daf00326f07f35c8f6bb6640862fc233
Rakesh Prajapati <mp966904@gmail.com>
2026-05-09 05:40:25 +0530

    Fix import case for Contact component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from './components/Hero'
 import Technologies from './components/Technologies'
 import Projects from './components/Projects'
 import Experience from './components/Experience'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
 import CursorGlow from "./components/CursorGlow";
